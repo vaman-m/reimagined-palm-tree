8bc6cd2514cf8f432ac7177b063fdcf539178684
nkanama <nkanama@umich.edu>
2022-12-22 21:44:35 +0530

    Backend done

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import pyttsx3
 import pandas as pd
 import pyaudio
 
-
+#global variables for NLP
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
-
+#check CSV for tool and return data
 def queryDatabase(text):
     readCSV = pd.read_csv("dataOpen.csv")
     df = pd.DataFrame(readCSV)
     answer = df.loc[df['type'] == text]
     print(answer)
 
+#text to speech engine prompts the user
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+#speech to text engine gets user input
 def take_command():
     with sr.Microphone() as source:
         # read the audio data from the default microphone
@@ -27,6 +29,7 @@ def take_command():
         # convert speech to text
         command = listener.recognize_sphinx(audio_data)
         print(command)
+    #match user input to available dataset
     readCSV = pd.read_csv("dataOpen.csv")
     df = pd.DataFrame(readCSV)
     products = list(df.type)
@@ -35,10 +38,11 @@ def take_command():
     for x in sentence:
         if x in products:
             return x
-    return command
+    return "error"
 
 
 if __name__=="__main__":
+    #main function driver
     intro = " Hi i am your assistant."
     intro_part2 = "What tool do you need? Please wait one moment before speaking"
     outro = "Here is the price and a link to the website for purchase"
